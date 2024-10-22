e1548303c583db29e76f07a7ef9de72e61df2610
Mark Louie <markllouie00@gmail.com>
2024-10-23 00:52:25 +0800

    adding something

--- script.js
+++ script.js
@@ -10,4 +10,13 @@ for(var i = 0; i < listItems.length; i++){
     listItems[i].addEventListener("click", itemSelected);
 }
 
+function myFunction(){
+    var name = document.getElementById("name").value;
+    var location = document.getElementById("location").value;
+
+    console.log(name);
+    console.log(location);
+
+    alert("Hello " + name + " you reported in " + location);
+}
     
