d279358bd3e65b46bd9729490474e9bdd3ddf7c1
Mark Louie <markllouie00@gmail.com>
2024-12-18 18:21:04 +0800

    added user registration

--- script/script.js
+++ script/script.js
@@ -1,15 +1,5 @@
 var listItems = document.getElementsByClassName("selectedItem");
 
-function itemSelected() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-}
-
-for(var i = 0; i < listItems.length; i++){
-    listItems[i].addEventListener("click", itemSelected);
-}
-
 const navbar = document.getElementById('collapseWidthExample');
 const contentColumn = document.getElementById('content');
 const navbarCol = document.getElementById('navbarCol');
