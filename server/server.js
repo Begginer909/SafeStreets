d40a8602096c7ff199f84e50b4a6ea9e73915f1d
Mark Louie <markllouie00@gmail.com>
2024-12-27 10:13:38 +0800

    creating a seperate folder for admin and user

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const http = require('http');
+const bcrypt = require('bcrypt');
 const { Server } = require('socket.io');
 const cors = require('cors');
 
